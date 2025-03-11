f5646d9aab56b86460a23901d671041d26a89c96
ughrima <agrimajain223@gmail.com>
2025-03-11 16:07:14 +0530

    Counter added

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("this is a popup!")
+// console.log("this is a popup!")
+
+const startingMinutes=10;
+let time=startingMinutes*60;
+
+const countdownel=document.getElementById('countdown');
+
+setInterval(updateCountDown,1000);
+
+function updateCountDown(){
+    const minutes=Math.floor(time/60);
+    let seconds=time%60;
+    seconds=seconds<10?'0'+seconds:seconds;
+
+    countdownel.innerHTML=minutes+":"+seconds;
+    time--;
+}
