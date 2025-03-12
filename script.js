f104ae485c04a92c584070b0fc54373c0c4aca27
ughrima <agrimajain223@gmail.com>
2025-03-13 02:17:26 +0530

    changes to timer

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-// console.log("this is a popup!")
-/*
-const startingMinutes=10;
-let time=startingMinutes*60;
 
-const countdownel=document.getElementById('#countdown');
-
-setInterval(updateCountDown,1000);
-
-function updateCountDown(){
-    const minutes=Math.floor(time/60);
-    let seconds=time%60;
-    seconds=seconds<10?'0'+seconds:seconds;
-
-    countdownel.innerHTML=minutes+":"+seconds;
-    time--;
-}
-*/
 document.getElementById("startTimer").addEventListener("click", function(){
-    var timeLeft = 0.1;
+    var timeLeft = 10;
     let time = timeLeft*60;
     var timer = setInterval(function f1(){
         const minutes=Math.floor(time/60);
@@ -32,3 +15,16 @@ document.getElementById("startTimer").addEventListener("click", function(){
         }
     }, 1000);
 })
+
+let startValue = 0;
+
+document.getElementById("timerInc").addEventListener("click", function(){
+    startValue+= 5;
+    document.getElementById("result").innerHTML = startValue;
+})
+
+document.getElementById("timerDec").addEventListener("click", function(){
+    startValue-= 5;
+    document.getElementById("result").innerHTML = startValue;
+})
+
